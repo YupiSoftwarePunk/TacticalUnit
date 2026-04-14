4f6428d49ec2fc56b753af9e0d09a5463307f78a
Yupi <youngperchik2404@gmail.com>
2026-04-14 16:37:12 +0500

    proj reconfigured in next

---
+++ Client/.storybook/main.ts
@@ -0,0 +1,34 @@
+import type { StorybookConfig } from '@storybook/nextjs-vite';
+import path from "path";
+
+const config: StorybookConfig = {
+  stories: [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  addons: [
+    "@chromatic-com/storybook",
+    "@storybook/addon-essentials",
+    "@storybook/addon-vitest",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs",
+    "@storybook/addon-onboarding"
+  ],
+  framework: {
+    name: "@storybook/nextjs-vite",
+    options: {},
+  },
+  staticDirs: ["../public"],
+
+  async viteFinal(config) {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "@": path.resolve(__dirname, "../src"),
+      };
+    }
+    return config;
+  },
+};
+
+export default config;
