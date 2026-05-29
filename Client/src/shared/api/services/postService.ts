4ce6e466ff89d9065d5cac39fbbde9290edad9a3
Yupi <youngperchik2404@gmail.com>
2026-05-29 23:32:23 +0500

    create template for api layer

---
+++ Client/src/shared/api/services/postService.ts
@@ -0,0 +1,7 @@
+
+
+export const PostService = {
+    // getAll: () => apiClient<Post[]>("/post"),
+    // getById: (id: number) => apiClient<Post>(`/post/${id}`),
+    // это тут для примера, как обращаться с ендпоинтами
+};
