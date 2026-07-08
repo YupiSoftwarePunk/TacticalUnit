0363c9eb371465faa160f0a5ecdf46de4652913d
Yupi <youngperchik2404@gmail.com>
2026-07-08 23:52:29 +0500

    fix profile #23

--- Client/src/app/profile/[DiscordId]/page.tsx
+++ Client/src/app/profile/[DiscordId]/page.tsx
@@ -43,10 +43,9 @@ export default function Profile({ params }: { params: Promise<{ DiscordId: strin
             <div className="flex min-h-[200px] h-[25vh] md:min-h-[300px] md:h-[30vh] bg-black relative">
                 <ProfileBGImage discordId={DiscordId} canEdit={true}></ProfileBGImage>
             </div>
+            <div className="flex flex-col md:flex-row flex-1 justify-center py-4 px-2 max-w-[1400px] mx-auto w-full gap-3">
 
-            <div className="flex flex-col md:flex-row flex-1 justify-center py-8 px-4 max-w-[1400px] mx-auto w-full gap-6">
-
-                <div className="flex justify-center md:justify-end">
+                <div className="flex justify-center md:justify-end md:w-1/4 md:flex-shrink-0">
                     <BaseContainer>
                         <ProfileSidePanel availableOptions={availableOptions}></ProfileSidePanel>
                     </BaseContainer>
@@ -54,54 +53,50 @@ export default function Profile({ params }: { params: Promise<{ DiscordId: strin
 
                 <div className="border-b md:border-r border-border-secondary w-full md:w-auto md:self-stretch"></div>
 
-                <div className="flex flex-col flex-1 gap-6">
-                    <div className="flex flex-col lg:flex-row gap-6 flex-1">
-                        <div className="flex flex-col flex-1 gap-2 min-w-0">
-                            <BaseContainer className="flex flex-1 p-4">
-                                <div className="flex flex-col w-full">
-                                    <UnitInfoPanel Unit={unitData}></UnitInfoPanel>
-                                </div>
-                            </BaseContainer>
-
-                            <BaseContainer className="flex ">
-                                <div className="w-full overflow-x-auto flex">
-                                    <div className="mx-auto min-w-max">
-                                        <ActivityCalendar UnitDiscordId={`${DiscordId}`}></ActivityCalendar>
-                                    </div>
-                                </div>
-                            </BaseContainer>
+                <div className="flex flex-col md:w-3/5 flex-grow min-w-0 gap-6">
+                    <BaseContainer className="flex flex-1 p-4">
+                        <div className="flex flex-col w-full">
+                            <UnitInfoPanel Unit={unitData}></UnitInfoPanel>
                         </div>
+                    </BaseContainer>
 
-                        <div className="border-b lg:border-r border-border-secondary w-full lg:w-auto lg:self-stretch"></div>
-
-                        <div className="flex ">
-                            <BaseContainer className="flex flex-col justify-start gap-4 p-4 w-full max-w-sm">
-                                <div className="flex flex-col gap-2 items-center lg:items-start">
-                                    <div className="w-full max-w-[320px] aspect-square border-b border-border-primary overflow-hidden flex justify-center items-center">
-                                        <StaticImage
-                                            type="kit"
-                                            entityId={DiscordId} 
-                                            alt="Избранный кит бойца"
-                                            className="object-top object-cover w-full h-full text-white bg-transparent"
-                                        />
-                                    </div>
-                                    <p className="text-text-secondary text-sm pl-1 self-start">Избранный кит профиля</p>
-                                </div>
-                                
-                                <div className="flex flex-col w-full items-center lg:items-start">
-                                    {unitData?.assignedRewardsIds && unitData?.assignedRewardsIds.length > 0 ? (
-                                        <div className="flex flex-wrap gap-1 justify-center lg:justify-start max-w-[308px]">
-                                            {unitData?.assignedRewardsIds.map(ri => (
-                                                <RewardDisplay key={ri} rewardId={ri}></RewardDisplay>
-                                            ))}
-                                        </div>
-                                    ) : (
-                                        <p className="text-sm italic">Боец еще не был награждён</p>
-                                    )}
-                                </div>
-                            </BaseContainer>
+                    <BaseContainer className="flex ">
+                        <div className="w-full overflow-x-auto flex">
+                            <div className="mx-auto min-w-max">
+                                <ActivityCalendar UnitDiscordId={`${DiscordId}`}></ActivityCalendar>
+                            </div>
                         </div>
-                    </div>
+                    </BaseContainer>
+                </div>
+
+                <div className="border-b md:border-r border-border-secondary w-full md:w-auto md:self-stretch"></div>
+
+                <div className="flex md:w-1/4 md:flex-shrink-0 justify-center">
+                    <BaseContainer className="flex flex-col justify-start gap-4 p-4 w-full max-w-sm">
+                        <div className="flex flex-col gap-2 items-center lg:items-start">
+                            <div className="w-full max-w-[320px] aspect-square border-b border-border-primary overflow-hidden flex justify-center items-center">
+                                <StaticImage
+                                    type="kit"
+                                    entityId={DiscordId} 
+                                    alt="Избранный кит бойца"
+                                    className="object-top object-cover w-full h-full text-white bg-transparent"
+                                />
+                            </div>
+                            <p className="text-text-secondary text-sm pl-1 self-start">Избранный кит профиля</p>
+                        </div>
+                        
+                        <div className="flex flex-col w-full items-center lg:items-start">
+                            {unitData?.assignedRewardsIds && unitData?.assignedRewardsIds.length > 0 ? (
+                                <div className="flex flex-wrap gap-1 justify-center lg:justify-start max-w-[308px]">
+                                    {unitData?.assignedRewardsIds.map(ri => (
+                                        <RewardDisplay key={ri} rewardId={ri}></RewardDisplay>
+                                    ))}
+                                </div>
+                            ) : (
+                                <p className="text-sm italic">Боец еще не был награждён</p>
+                            )}
+                        </div>
+                    </BaseContainer>
                 </div>
             </div>
         </div>
